1943fd05f3eba573a9a8ae331565e656b8f2128d
Michael Granados <michaelgranados@gmail.com>
2014-04-05 19:03:31 -0300

    Creating the plugin file, with the description tags
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

---
+++ oembed-as-media.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: oEmbed as media
+ * Plugin URI: http://github.com/dgmike/oembed-as-media
+ * Description: Creates an media in your library with an youtube, vimeo, hulu, and another oembed media. The plugin will fetch the data and create an media item with this data.
+ * Version: $version$
+ * Author: dgmike
+ * Author URI: http://dgmike.com.br
+ * License: GPLv2 or later
+ */
+
